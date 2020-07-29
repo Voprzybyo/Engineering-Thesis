bfb0c5cf600b3b7e749bed332c0849b92117b216
Wojtek <wo.przybyo@gmail.com>
2020-07-29 19:48:31 +0200

    ThingsBoard code update

--- ThingsBoard_Temp_Hum/App_MyApp_MeshRouter_DB.c
+++ ThingsBoard_Temp_Hum/App_MyApp_MeshRouter_DB.c
@@ -1,31 +1,3 @@
-/*****************************************************************************
- * Copyright ©2019. Radiocrafts AS (Radiocrafts).  All Rights Reserved. 
- * Permission to use, copy, modify, and distribute this software and 
- * its documentation, without fee and without a signed licensing 
- * agreement, is hereby granted, provided that the above copyright 
- * notice, this paragraph and the following two paragraphs appear in 
- * all copies, modifications, and distributions.
- * 
- * IN NO EVENT SHALL RADIOCRFTS BE LIABLE TO ANY PARTY FOR DIRECT, 
- * INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES, INCLUDING 
- * LOST PROFITS, ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS 
- * DOCUMENTATION, EVEN IF RADIOCRAFTS HAS BEEN ADVISED OF THE 
- * POSSIBILITY OF SUCH DAMAGE. 
- * 
- * RADIOCRAFTS SPECIFICALLY DISCLAIMS ANY WARRANTIES, INCLUDING, BUT 
- * NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND 
- * FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE AND ACCOMPANYING 
- * DOCUMENTATION, IF ANY, PROVIDED HEREUNDER IS PROVIDED "AS IS". 
- * RADIOCRAFTS HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, 
- * UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
-****************************************************************************/
-
-/**
- @file
- @brief This is the Development Board (DB) firmware for starting as a mesh router
- @version 1.00 : First revision
- @date 2019.02.25
-*/
 
 #include "RIIM_UAPI.h"
 
